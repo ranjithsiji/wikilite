34f77aa9a5c3fb40b0a85ecbfef8841cea01dbb5
Ranjithsiji <ranjith.sajeev@gmail.com>
2015-03-20 15:59:40 +0530

    Adding the Project Files

--- README.md
+++ README.md
@@ -1,11 +1,69 @@
-#Wikilite: 
-==========
-An attempt to create a fast and lightweight access to Wikipedia.
+#Wikilite 
+----------
+- Wikilite: An attempt to create a fast and lightweight access to Wikipedia.
 (It is buggy and it is a rough idea. please use with care)
 
 Wikilite is a very small programm (HTML/CSS/Javascript/Jquery) to create a simple frontend search to wikipedia. Just to access wikipedia faster. It is a rought idea to get into the right wikipedia article very quickly.
 
-#How to use it
+
+##How to use it
+-------------------
 Just download the files and open the index.html in your favourite browser. Make sure that your internet connnection is working and start searching. 
 Please see index.html for the programme.
+
 wikilite depend on jQuery. But we can make a non jQuery version also.
+
+##Demo
+----
+Just go to [http://wikilite.nd4.org/]
+
+
+##Contributors
+-------------------
+
+Ranjith Siji [https://en.wikipedia.org/wiki/User:Ranjithsiji]
+
+
+##My Blog
+-------
+Smashing Web [http://smashingweb.info]
+
+##About Wikilite
+--------------
+
+The Idea of creating a lightweight search frondend for wikipedia came from the question [https://meta.wikimedia.org/wiki/Talk:2015_Strategy/Community_consultation] . Will wikipedia survive for next billion users?. 
+
+Also I stuck on the websites [http://www.wikipediaredefined.com] and [http://wikipedia.gkvasnikov.com/]. They provide new ideas to make wikipedia better. 
+
+In a small talk Viswaettan [http://en.wikipedia.org/wiki/User:Viswaprabha] told that the new visual editior in wikipedia is dead slow. Also I feel that wikipedia is slow to load on some poor internet connections. The average bandwidth in India is 2MBPS (Lowest in Asia-Pacific) . In rural India the connection speed is much slower. So creating a quick access to wikipedia is a great Idea. The whole programme is just 120KB (Wikipedia Home Page is 80KB !!!). It is because of jQuery. It is possible to create this thing without jQuery. But this is a quick hack and created from a quick idea. So I used jquery for quick development. 
+
+On the other side is you can download the whole pacakge in a zip file and just open the index.html with your favourite browser and go on. 
+
+The whole project is build around 3 mediawiki apis. One Opensearch and two query. This is a rough sketch and lot of unfinished parts can be found in this. 
+
+###Cons of Wikilite
+
+I think the whole wikilite project is against the idea of anybody can contribute to wikipedia. Wikilite is extracting the information from wikipedia and present in a faster way. But to edit you must go to wikipedia itself. So it is against the collaborative property of wikipedia. But we can make reach to more people and make it better. 
+
+
+##Usage of Wikilite
+-----------------
+In the big textbox you can start typing. An autocomplete box will appear with suggestions from wikipieda like google suggest. If you can move mouse over the line a short description will appear on the rightside . A link to wikipedia article is also included in that. If you want to search with a text in wikipedia then click on search button. A search result (10 Results) will be presented below the big input box. If you click readmore then the text will be presented in distraction free reading mode.
+
+###Distraction Free Reading
+------------------------
+Distraction free reading mode created to read the content without any other distractions on the website. No Images, no links, no Infobox, only content. In wikipedia usually you stuck with lot of images internlinks (The better way to connect information) but here you and the content only. You can always go to wikipedia for full article. 
+
+This is created from the Wikitextextract. So lot of limitation is there. But this is only a concept. Can improve later.
+
+##Credits
+-------
+
+Manojk [http://ml.wikisource.org/User:Manojk], Viswettan[http://en.wikipedia.org/wiki/User:Viswaprabha], SMC[http://smc.org.in], Santhosh Thottingal [http://wikimediafoundation.org/wiki/User:Sthottingal-WMF] Anivar [http://en.wikipedia.org/wiki/User:AniVar], and other friends in wikipedia and SMC. 
+
+##License
+---------
+
+GNU GPL v3 is the license except jQuery. for jQuery license go [https://jquery.org/license/]
+
+
---
+++ js/jqmain.js
@@ -0,0 +1,120 @@
+jQuery( document ).ready(function($) {
+	$('#searchBtn').addClass("searchRun");
+	$('#sForm').removeClass("loadingmessage");
+	$('#retBox').hide();
+	console.log( "System Ready" ); // The Jurassic Park System Ready.
+	//Adding Delay dont hurt wikipedia api.
+	$('#searchInput').on("input", throttle (function(tval){ 
+		$('#retBox').hide().empty();
+       	$("#retInfoBox").hide().empty();
+       	$("#resBtn").hide().empty();
+		 //console.log($(this).val().length);
+		if ( $.trim($(this).val()) || tval.which !== 0 && !tval.ctrlKey && !tval.metaKey && !tval.altKey) {
+        	//console.log($(this).val());
+        	 var wikiurl = "http://en.wikipedia.org/w/api.php?action=opensearch&search="+$(this).val()+"&format=json&callback=?";
+        	 $('#sForm').addClass("loadingmessage");
+				$.ajax({ 
+					dataType: "json",
+					url: wikiurl,
+					})
+				.done(function( data ) {
+					$('#retBox').empty(); $("#retInfoBox").hide().empty();
+					if (data.length !=0) {
+						var dpos = $('#searchInput').offset();
+						$('#retBox').css({"left":dpos.left+"px","top": (dpos.top+37) + "px"});
+						$.each(data[1], function(i,item){
+							//console.log(item);
+							$('#retBox').append('<div class="dropInfo"><p class="dropInfoP">'+item+'</p><p class="dropInfoDesc">'+data[2][i]+'<br/><a class="barLink" href="'+data[3][i]+'">Wikipedia-></a></p></div>');
+						});
+						$('#sForm').removeClass("loadingmessage");
+						$('#retBox').show();
+						var leftpos = $("#retBox").offset().left + $("#retBox").outerWidth();
+						$("#retInfoBox").css({"left":(leftpos)+"px","top":($("#retBox").offset().top)+"px"})
+										.show();
+					}
+				});
+		}	
+	},500));
+	//Hover Just display More Information
+	$('#retBox').on('mouseover', '.dropInfo', function (event) { 
+	    var intxt=$(this).find(".dropInfoDesc").html();
+	    
+	    //alert ($(this).find(".dropInfoDesc").html());
+	    $("#retInfoBox").html(intxt)
+	    				.show();
+
+	});	
+	//Take value to Input
+	$('#retBox').on('click', '.dropInfo', function (event) {	
+		$('#retBox').hide(); $("#retInfoBox").hide();
+		$('#searchInput').val($(this).find(".dropInfoP").html());
+		$('#resBtn').html('<a title="Go to Wikipedia Article" href="'+$(this).find(".barLink").attr("href")+'">GO</a>');
+		$("#resBtn").show();
+	});
+	//Search on Wikipedia
+	$('#searchBtn').on('click', $(this), function (event) {	
+		$('#retBox').hide(); $("#retInfoBox").hide(); 
+		$('#searchBtn').css({"background-color":"#efbe5c","color":"#888"}).animate({"background-color":"#efbe5c","color":"#888"}, 1000);
+		$('#sForm').addClass("loadingmessage");
+		$("#resBtn").hide().empty();
+		var wiki2="http://en.wikipedia.org/w/api.php?action=query&list=search&srsearch="+$('#searchInput').val()+"&srwhat=text&format=json&callback=?";
+		var wikigourl='http://en.wikipedia.org/wiki/';
+		$.ajax({ 
+					dataType: "json",
+					url: wiki2,
+				})
+				.done (function( data ) {
+					$('#resltBox').html(data[2]);
+					$('#searchBtn').css({"background-color":"#2196f3","color":"#f1f1f1"});
+					$('#sForm').removeClass("loadingmessage");
+					$('#resltBox').empty();
+					$.each(data.query.search, function(i,item){
+						$('#resltBox').append (
+								'<div class="rslBox"><h2><a href="'+wikigourl+item.title+'">'+item.title+'</a></h2><p>'+item.snippet+' ...</p><p><a class="readOn" title="'+item.title+'"href="'+wikigourl+item.title+'">read on Wikipedia &raquo;</a> | <a class="dirRead">Read More</a></p><p class="timeS">Time: '+item.timestamp+' | No of Words: '+item.wordcount+'</p>'+'</div>'
+							);
+					});
+				});
+
+	});
+	//Distraction Free Reading Mode.
+	$('#resltBox').on('click','.dirRead', function (event) { 
+		var goTitle = $(this).parent().find(".readOn").attr("title");
+		var wikiUrls=$(this).parent().find(".readOn").attr("href");
+		var goUrl = "http://en.wikipedia.org/w/api.php?action=query&prop=extracts&exsectionformat=wiki&titles="+goTitle+"&redirects=true&format=json&callback=?";
+		$('#loadingBox ').show();
+		$.ajax ({
+				dataType: "json",
+				url: goUrl, 
+		})
+		.done (function( data ) {
+			$.each(data.query.pages, function(i,item){
+				$('#disReadBoxIn').html (
+						'<h2><a href="'+wikiUrls+'">'+item.title+'</a></h2>'+item.extract
+					);
+			});
+			$('#disReadBox').show();
+			$('#resltBox').hide();
+			$('#loadingBox ').hide();
+		});
+	});
+	//Close Overlay mode
+	$('#closeBtn').click(function () { 
+		$('#disReadBox').hide();
+		$('#resltBox').show();
+	});
+  	//The Delay Function to control api request frequency.
+  	function throttle(f, delay){ 
+	    var timer = null;
+	    return function(){
+	        var context = this, args = arguments;
+	        clearTimeout(timer);
+	        timer = window.setTimeout(function(){
+	            f.apply(context, args);
+	        },
+	        delay || 500);
+	    };
+	}  
+    
+});
+
+
